84f6afce954f748177687b880ef8f89235bf07ae
dafatov <dafatov@yandex.ru>
2025-03-15 18:33:42 +0300

    bugfix(#50) - Added RateLimiterWrapper - Added rpm & rps for shikimori client - Rearranged option's choices in shikimori command - Turned off some logs in local profile

---
+++ src/main/java/ru/demetrious/deus/bot/adapter/output/shikimori/limiter/RpmRateLimiter.java
@@ -0,0 +1,20 @@
+package ru.demetrious.deus.bot.adapter.output.shikimori.limiter;
+
+import io.github.resilience4j.retry.annotation.Retry;
+import java.util.function.Function;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import ru.demetrious.deus.bot.adapter.output.shikimori.ShikimoriClient;
+import ru.demetrious.deus.bot.domain.limiter.Limiter;
+
+@RequiredArgsConstructor
+@Component
+@Order(2)
+public class RpmRateLimiter implements Limiter<ShikimoriClient> {
+    @Retry(name = "shikimori-rpm")
+    @Override
+    public <R, A> Function<R, A> execute(Function<R, A> function) {
+        return Limiter.super.execute(function);
+    }
+}
---
+++ src/main/java/ru/demetrious/deus/bot/domain/limiter/RateLimiterWrapper.java
@@ -0,0 +1,22 @@
+package ru.demetrious.deus.bot.domain.limiter;
+
+import java.util.List;
+import java.util.function.Function;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class RateLimiterWrapper<T> {
+    private final List<Limiter<T>> limiterList;
+
+    public <R, A> Function<R, A> wrap(Function<R, A> function) {
+        Function<R, A> tmp = function;
+
+        for (Limiter<T> limiter : limiterList) {
+            tmp = limiter.execute(tmp);
+        }
+
+        return function;
+    }
+}
