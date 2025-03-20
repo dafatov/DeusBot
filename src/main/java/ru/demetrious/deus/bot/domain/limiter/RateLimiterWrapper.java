a3f0b2c237b1f88ef3231888821ff5d78207affe
dafatov <dafatov@yandex.ru>
2025-03-20 19:38:19 +0300

    bugfix(#50) - Added RateLimiterWrapper - Added rpm & rps for shikimori client - Rearranged option's choices in shikimori command - Turned off some logs in local profile

--- src/main/java/ru/demetrious/deus/bot/domain/limiter/RateLimiterWrapper.java
+++ src/main/java/ru/demetrious/deus/bot/domain/limiter/RateLimiterWrapper.java
@@ -1,5 +1,6 @@
 package ru.demetrious.deus.bot.domain.limiter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.github.resilience4j.ratelimiter.RateLimiterConfig;
 import io.github.resilience4j.ratelimiter.RateLimiterRegistry;
 import io.github.resilience4j.retry.RetryConfig;
@@ -7,6 +8,7 @@ import io.github.resilience4j.retry.RetryRegistry;
 import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import ru.demetrious.deus.bot.utils.JacksonUtils;
 
 import static io.github.resilience4j.core.IntervalFunction.ofExponentialRandomBackoff;
 import static io.github.resilience4j.ratelimiter.RateLimiter.decorateFunction;
@@ -26,7 +28,15 @@ public class RateLimiterWrapper<T> {
         .addRateLimiterConfig("rpm", RateLimiterConfig.custom()
             .limitForPeriod(72)
             .limitRefreshPeriod(ofMinutes(1))
-            .timeoutDuration(ofSeconds(2))
+            .timeoutDuration(ofMinutes(2))
+            .drainPermissionsOnResult(either -> {
+                try {
+                    JacksonUtils.getMapper().writeValueAsString(either);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+                return false;
+            })
             .build())
         .build();
     //private final List<Limiter<T>> limiterList;
@@ -35,7 +45,8 @@ public class RateLimiterWrapper<T> {
         RetryRegistry retryRegistry = RetryRegistry.custom()
             .addRetryConfig("retry", RetryConfig.custom()
                 .maxAttempts(6)
-                .intervalFunction(ofExponentialRandomBackoff(ofSeconds(1), 2, ofMinutes(5)))
+                .intervalFunction(ofExponentialRandomBackoff(ofSeconds(2), 2, ofMinutes(5)))
+                .failAfterMaxAttempts(true)
                 .build())
             .build();
 
