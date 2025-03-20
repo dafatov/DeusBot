fef6ec137999bb0457fa889a96f7860227e36ce7
dafatov <dafatov@yandex.ru>
2025-03-20 23:09:35 +0300

    bugfix(#50) - Added RateLimiterWrapper - Added rpm & rps for shikimori client - Rearranged option's choices in shikimori command - Turned off some logs in local profile

--- src/main/java/ru/demetrious/deus/bot/adapter/output/shikimori/ShikimoriClient.java
+++ src/main/java/ru/demetrious/deus/bot/adapter/output/shikimori/ShikimoriClient.java
@@ -2,6 +2,7 @@ package ru.demetrious.deus.bot.adapter.output.shikimori;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.demetrious.deus.bot.domain.graphql.Request;
@@ -19,6 +20,7 @@ import static ru.demetrious.deus.bot.fw.config.security.AuthorizationComponent.S
 public interface ShikimoriClient {
     @RateLimiter(name = "test")
     @Bulkhead(name = "test")
+    @Retry(name = "test")
     @PostMapping
     Response execute(Request requestShikimori);
 }
