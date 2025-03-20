e667f4460949b75be798892950348dccf2d0e94e
dafatov <dafatov@yandex.ru>
2025-03-20 14:41:21 +0300

    bugfix(#50) - Added RateLimiterWrapper - Added rpm & rps for shikimori client - Rearranged option's choices in shikimori command - Turned off some logs in local profile

--- src/main/java/ru/demetrious/deus/bot/adapter/output/shikimori/limiter/RpmRateLimiter.java
+++ src/main/java/ru/demetrious/deus/bot/adapter/output/shikimori/limiter/RpmRateLimiter.java
@@ -1,6 +1,7 @@
 package ru.demetrious.deus.bot.adapter.output.shikimori.limiter;
 
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.annotation.Order;
@@ -13,6 +14,7 @@ import ru.demetrious.deus.bot.domain.limiter.Limiter;
 @Order(2)
 public class RpmRateLimiter implements Limiter<ShikimoriClient> {
     @RateLimiter(name = "shikimori-rpm")
+    @Retry(name = "shikimori-rpm")
     @Override
     public <R, A> Function<R, A> execute(Function<R, A> function) {
         return Limiter.super.execute(function);
