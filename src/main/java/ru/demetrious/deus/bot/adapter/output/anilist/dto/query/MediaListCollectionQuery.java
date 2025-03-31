d7e3f7156aaec4c8c53e335153334450b2ef6f95
dafatov <dafatov@yandex.ru>
2025-03-31 23:13:23 +0300

    bugfix(#57) - Added retry on anilist client - Fixed missed animes in getOldAnime method

--- src/main/java/ru/demetrious/deus/bot/adapter/output/anilist/dto/query/MediaListCollectionQuery.java
+++ src/main/java/ru/demetrious/deus/bot/adapter/output/anilist/dto/query/MediaListCollectionQuery.java
@@ -1,19 +1,21 @@
 package ru.demetrious.deus.bot.adapter.output.anilist.dto.query;
 
-import lombok.AllArgsConstructor;
-import ru.demetrious.deus.bot.adapter.output.anilist.dto.enums.MediaTypeAnilist;
+import lombok.RequiredArgsConstructor;
 import ru.demetrious.deus.bot.domain.graphql.Query;
+
+import static ru.demetrious.deus.bot.adapter.output.anilist.dto.enums.MediaTypeAnilist.ANIME;
 
 /**
  * GraphQl query dto
  */
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MediaListCollectionQuery implements Query {
-    private Integer userId;
-    private MediaTypeAnilist type;
+    private final Integer userId;
+    private final Integer chunk;
 
     @Override
     public String serialize() {
-        return "MediaListCollection(userId:%d,type:%s){lists{entries{id,media{idMal},progress,repeat,score,status}}}".formatted(userId, type);
+        return "MediaListCollection(userId:%d,type:%s,chunk:%s,perChunk:%s){lists{entries{id,media{idMal},progress,repeat,score,status}}}"
+            .formatted(userId, ANIME, chunk, 500);
     }
 }
