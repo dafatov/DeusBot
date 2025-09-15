b262bdf404953a93c2601b2a5b34608bef2e0246
dafatov <dafatov@yandex.ru>
2025-09-15 21:38:17 +0300

    bugfix(#76): - Исправил out of memory при генерации webp - Отрефакторил генерацию webp - Перевел нативный webp на ненативный

--- src/main/java/ru/demetrious/deus/bot/utils/DefaultUtils.java
+++ src/main/java/ru/demetrious/deus/bot/utils/DefaultUtils.java
@@ -7,6 +7,14 @@ import org.apache.commons.lang3.function.FailableSupplier;
 @Log4j2
 @UtilityClass
 public class DefaultUtils {
+    public static <T> T throwIfException(FailableSupplier<T, Throwable> supplier) {
+        try {
+            return supplier.get();
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static <T> T defaultIfException(FailableSupplier<T, Throwable> supplier) {
         return defaultIfException(supplier, null);
     }
