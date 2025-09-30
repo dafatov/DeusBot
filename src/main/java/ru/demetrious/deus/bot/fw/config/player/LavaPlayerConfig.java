e87eaaeee797a1eb467d31dd3ef16038207da5e6
dafatov <33876950+dafatov@users.noreply.github.com>
2025-09-30 19:48:42 +0300

    На некоторых видео ютуба падает и скипает #65 (#126)
    * На некоторых видео ютуба падает и скипает #65 (#125)
    - Обновлена версия библиотеки lavalink.youtube-common
    
    * На некоторых видео ютуба падает и скипает #65
    - Добавлены все возможные клиенты youtube в AudioSourceManager
    - Изменен cron на очистку образов докера в docker-cleanup.yaml
    
    ---------
    
    Co-authored-by: MaKeMaK-lee <73843102+MaKeMaK-lee@users.noreply.github.com>

--- src/main/java/ru/demetrious/deus/bot/fw/config/player/LavaPlayerConfig.java
+++ src/main/java/ru/demetrious/deus/bot/fw/config/player/LavaPlayerConfig.java
@@ -6,8 +6,15 @@ import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.source.http.HttpAudioSourceManager;
 import dev.lavalink.youtube.YoutubeAudioSourceManager;
+import dev.lavalink.youtube.clients.Android;
+import dev.lavalink.youtube.clients.AndroidMusic;
+import dev.lavalink.youtube.clients.AndroidVr;
+import dev.lavalink.youtube.clients.Ios;
+import dev.lavalink.youtube.clients.MWeb;
 import dev.lavalink.youtube.clients.Music;
+import dev.lavalink.youtube.clients.Tv;
 import dev.lavalink.youtube.clients.TvHtml5Embedded;
+import dev.lavalink.youtube.clients.WebEmbedded;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,7 +46,18 @@ public class LavaPlayerConfig {
     // ===================================================================================================================
 
     private AudioSourceManager[] getAudioSourceManagers() {
-        YoutubeAudioSourceManager youtubeAudioSourceManager = new YoutubeAudioSourceManager(new Music(), new Web(), new TvHtml5Embedded());
+        YoutubeAudioSourceManager youtubeAudioSourceManager = new YoutubeAudioSourceManager(
+            new Web(),
+            new Android(),
+            new AndroidMusic(),
+            new AndroidVr(),
+            new Ios(),
+            new MWeb(),
+            new Music(),
+            new Tv(),
+            new TvHtml5Embedded(),
+            new WebEmbedded()
+        );
 
         youtubeAudioSourceManager.useOauth2(youtubeRefreshToken, isNotBlank(youtubeRefreshToken));
 
