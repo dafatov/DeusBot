3e839f76e4e23aa4a70d6e0ef83269a546092f99
dafatov <33876950+dafatov@users.noreply.github.com>
2025-10-04 20:45:23 +0300

    Актуализировать список баннеров reverse1999 #128 (#129)
    - Добавлена env'а REVERSE1999_POOLS с мапой наименований баннеров reverse 1999

--- src/main/java/ru/demetrious/deus/bot/app/impl/command/Reverse1999PullsShowCommandUseCase.java
+++ src/main/java/ru/demetrious/deus/bot/app/impl/command/Reverse1999PullsShowCommandUseCase.java
@@ -1,12 +1,16 @@
 package ru.demetrious.deus.bot.app.impl.command;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.stereotype.Component;
 import ru.demetrious.deus.bot.app.api.character.GetReverseCharacterListOutbound;
@@ -25,11 +29,13 @@ import ru.demetrious.deus.bot.domain.MessageFile;
 import ru.demetrious.deus.bot.domain.PullsData;
 import ru.demetrious.deus.bot.fw.config.security.AuthorizationComponent;
 
+import static java.util.Optional.empty;
+import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.groupingBy;
 import static ru.demetrious.deus.bot.domain.CommandData.Name.REVERSE1999_PULLS_IMPORT;
 import static ru.demetrious.deus.bot.domain.CommandData.Name.REVERSE1999_PULLS_SHOW;
-import static ru.demetrious.deus.bot.domain.Pull.COLLABORATION_POOL_TYPE;
 import static ru.demetrious.deus.bot.fw.config.security.AuthorizationComponent.GOOGLE_REGISTRATION_ID;
+import static ru.demetrious.deus.bot.utils.JacksonUtils.getMapper;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -40,6 +46,7 @@ public class Reverse1999PullsShowCommandUseCase implements Reverse1999ShowComman
     private final FindPullsDataOutbound findPullsDataOutbound;
     private final AuthorizationComponent authorizationComponent;
     private final GetUserIdOutbound<SlashCommandInteractionInbound> getUserIdOutbound;
+    private final PoolNameComponent poolNameComponent;
 
     @Override
     public CommandData getData() {
@@ -62,11 +69,9 @@ public class Reverse1999PullsShowCommandUseCase implements Reverse1999ShowComman
         List<MessageFile> messageFileList = findPullsDataOutbound.findPullsData()
             .map(PullsData::getPullList).stream()
             .flatMap(Collection::stream)
-            .collect(groupingBy(pull -> Objects.equals(pull.getPoolType(), COLLABORATION_POOL_TYPE)
-                ? new GroupKey(false, pull.getPoolId())
-                : new GroupKey(true, pull.getPoolType())))
+            .collect(groupingBy(pull -> new GroupKey(pull.getPoolType(), poolNameComponent.hasOnlyType(pull.getPoolType()) ? null : pull.getPoolId())))
             .entrySet().stream()
-            .map(entry -> new ReversePullTypeCanvas(entry.getValue(), characterMap, entry.getKey()))
+            .map(entry -> new ReversePullTypeCanvas(entry.getValue(), characterMap, entry.getKey(), poolNameComponent::getName))
             .map(ReversePullTypeCanvas::createFile)
             .toList();
         MessageData messageData;
@@ -79,5 +84,30 @@ public class Reverse1999PullsShowCommandUseCase implements Reverse1999ShowComman
             messageData = new MessageData().setFiles(messageFileList);
         }
         notifyOutbound.notify(messageData);
+    }
+
+    @Configuration
+    public static class PoolNameComponent {
+        private final Map<String, Object> pools;
+
+        public PoolNameComponent(@Value("${REVERSE1999_POOLS:{}}") String poolsJson) throws JsonProcessingException {
+            this.pools = getMapper().readValue(poolsJson, new TypeReference<>() {
+            });
+        }
+
+        public boolean hasOnlyType(int typeId) {
+            return pools.get(String.valueOf(typeId)) instanceof String;
+        }
+
+        public Optional<String> getName(@NotNull GroupKey groupKey) {
+            return switch (pools.get(String.valueOf(groupKey.typeId()))) {
+                case String name -> name.describeConstable();
+                case Map<?, ?> poolTypeMap -> ofNullable(groupKey.id())
+                    .map(String::valueOf)
+                    .map(poolTypeMap::get)
+                    .map(String::valueOf);
+                case null, default -> empty();
+            };
+        }
     }
 }
