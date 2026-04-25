1232adb31b2c83c152cf26e22e02882cf5703581
dafatov <33876950+dafatov@users.noreply.github.com>
2026-04-25 21:57:14 +0300

    Неподробный импорт шикимори на анилист #173 (#174)
    - Расширил информацию, выводимую пользаку об успешности импорта в анилист
    - Отрефачил отправку в анилист

--- src/main/java/ru/demetrious/deus/bot/app/impl/command/ShikimoriCommandUseCase.java
+++ src/main/java/ru/demetrious/deus/bot/app/impl/command/ShikimoriCommandUseCase.java
@@ -3,8 +3,11 @@ package ru.demetrious.deus.bot.app.impl.command;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.stereotype.Component;
@@ -17,6 +20,7 @@ import ru.demetrious.deus.bot.app.api.message.NotifyOutbound;
 import ru.demetrious.deus.bot.app.api.user.GetUserIdOutbound;
 import ru.demetrious.deus.bot.domain.CommandData;
 import ru.demetrious.deus.bot.domain.ImportAnimeContext;
+import ru.demetrious.deus.bot.domain.ImportAnimeContext.AnimeProjection;
 import ru.demetrious.deus.bot.domain.MessageData;
 import ru.demetrious.deus.bot.domain.MessageEmbed;
 import ru.demetrious.deus.bot.domain.MessageFile;
@@ -25,9 +29,19 @@ import ru.demetrious.deus.bot.domain.OptionData;
 import ru.demetrious.deus.bot.fw.config.security.AuthorizationComponent;
 
 import static java.lang.Integer.parseInt;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.time.Instant.now;
+import static java.util.Comparator.comparing;
 import static java.util.Optional.empty;
+import static java.util.stream.Collectors.joining;
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.StringUtils.LF;
+import static org.apache.commons.lang3.StringUtils.join;
+import static org.apache.commons.lang3.tuple.Pair.of;
+import static ru.demetrious.deus.bot.app.api.message.NotifyOutbound.DESCRIPTION_MAX_LENGTH;
 import static ru.demetrious.deus.bot.domain.CommandData.Name.SHIKIMORI;
+import static ru.demetrious.deus.bot.domain.MessageEmbed.ColorEnum.INFO;
 import static ru.demetrious.deus.bot.domain.OptionData.Type.STRING;
 import static ru.demetrious.deus.bot.fw.config.security.AuthorizationComponent.ANILIST_REGISTRATION_ID;
 import static ru.demetrious.deus.bot.fw.config.security.AuthorizationComponent.SHIKIMORI_REGISTRATION_ID;
@@ -39,6 +53,7 @@ public class ShikimoriCommandUseCase extends PlayerCommand implements ShikimoriC
     private static final String STRING_OPTION_METHOD = "method";
     private static final String METHOD_CHOICE_FILE = "file";
     private static final String METHOD_CHOICE_ANILIST = "anilist";
+    private static final String HTML_BODY_PATTERN = "<!DOCTYPE html><html lang=\"ru\"><head><meta charset=\"UTF-8\"><title>Статистика</title></head><body>%s</body></html>";
 
     private final GetAnimeOutbound getAnimeOutbound;
     private final GetUserIdOutbound<SlashCommandInteractionInbound> getUserIdOutbound;
@@ -110,16 +125,24 @@ public class ShikimoriCommandUseCase extends PlayerCommand implements ShikimoriC
             case METHOD_CHOICE_ANILIST -> {
                 String anilistPrincipalName = anilistUser.get().getPrincipalName();
                 ImportAnimeContext importAnimeContext = importAnimeOutbound.execute(getAnimeOutbound.getAnimeList(), parseInt(anilistPrincipalName));
+                String description = prettifyContext(importAnimeContext, group -> mapGroupMarkdown(group, false));
+                List<MessageFile> messageFileList = List.of();
 
-                yield new MessageData().setEmbeds(List.of(new MessageEmbed()
-                    .setColor(MessageEmbed.ColorEnum.INFO)
-                    .setTitle("Список shikimori успешно импортирован на anilist.co")
-                    .setUrl("%s/user/%s/animelist".formatted(anilistUrl, anilistPrincipalName))
-                    .setDescription("""
-                        Обновлено или добавлено: %d
-                        Удалено: %d
-                        """.formatted(importAnimeContext.getChangesCount(), importAnimeContext.getRemovedCount()))
-                    .setTimestamp(now())));
+                if (description.length() > DESCRIPTION_MAX_LENGTH) {
+                    description = prettifyContext(importAnimeContext, group -> mapGroupMarkdown(group, true));
+                    messageFileList = List.of(new MessageFile()
+                        .setName("shikimoriToAnilist.html")
+                        .setData(HTML_BODY_PATTERN.formatted(prettifyContext(importAnimeContext, ShikimoriCommandUseCase::mapGroupHtml)).getBytes(UTF_8)));
+                }
+
+                yield new MessageData()
+                    .setEmbeds(List.of(new MessageEmbed()
+                        .setColor(INFO)
+                        .setTitle("Список shikimori успешно импортирован на anilist.co")
+                        .setDescription(description)
+                        .setUrl("%s/user/%s/animelist".formatted(anilistUrl, anilistPrincipalName))
+                        .setTimestamp(now())))
+                    .setFiles(messageFileList);
             }
             default -> throw new IllegalStateException("Unexpected value: " + method);
         };
@@ -138,5 +161,42 @@ public class ShikimoriCommandUseCase extends PlayerCommand implements ShikimoriC
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static String prettifyContext(ImportAnimeContext importAnimeContext, Function<Pair<String, List<AnimeProjection>>, String> groupMapFunction) {
+        return Stream.of(
+                of("Добавлено", importAnimeContext.getAdded()),
+                of("Изменено", importAnimeContext.getEdited()),
+                of("Удалено", importAnimeContext.getRemoved()),
+                of("Пропущено", importAnimeContext.getSkipped()),
+                of("Остальное", importAnimeContext.getAnother()))
+            .map(groupMapFunction)
+            .collect(joining(LF));
+    }
+
+    private static String mapGroupMarkdown(Pair<String, List<AnimeProjection>> group, boolean isPreview) {
+        String header = "%s%s: %d".formatted(isPreview ? EMPTY : "### ", group.getLeft(), group.getRight().size());
+
+        if (isEmpty(group.getRight()) || isPreview) {
+            return header;
+        }
+
+        return join(header, LF, group.getRight().stream()
+            .sorted(comparing(AnimeProjection::getTitle))
+            .map(a -> "-# [%s](%s)".formatted(a.getTitle(), a.getUrl()))
+            .collect(joining(LF)));
+    }
+
+    private static String mapGroupHtml(Pair<String, List<AnimeProjection>> group) {
+        String header = "<h3>%s: %d</h3>".formatted(group.getLeft(), group.getRight().size());
+
+        if (isEmpty(group.getRight())) {
+            return header;
+        }
+
+        return join(header, "<ul>", group.getRight().stream()
+            .sorted(comparing(AnimeProjection::getTitle))
+            .map(a -> "<li><a href=\"%s\">%s</a></li>".formatted(a.getUrl(), a.getTitle()))
+            .collect(joining()), "</ul>");
     }
 }
